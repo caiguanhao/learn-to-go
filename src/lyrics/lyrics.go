45faaa8d679c026ae29903dea2d9f606efc74c49
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-09-04 21:12:11 +0800

    move code to different files;

--- src/lyrics/lyrics.go
+++ src/lyrics/lyrics.go
@@ -1,19 +1,18 @@
 package main
 
 import (
+	"./providers/azlyrics"
+	"./structs"
 	"bytes"
 	"flag"
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
 	"io"
 	"io/ioutil"
-	"net/url"
 	"os"
 	"os/exec"
 	"os/signal"
 	"os/user"
 	"path"
-	"regexp"
 	"strings"
 	"time"
 )
@@ -30,7 +29,7 @@ var (
 	noPager bool
 	pager   bool
 
-	currentTrack *Track
+	currentTrack *structs.Track
 
 	osascript = `
 	if application "iTunes" is running then
@@ -44,58 +43,6 @@ var (
 	userHomeDir    string
 	lyricsCacheDir string
 )
-
-type (
-	Track struct {
-		Name   string
-		Artist string
-	}
-
-	Result struct {
-		URL       string
-		TrackName string
-	}
-)
-
-func (track Track) Convert() *Track {
-	name := track.Name
-	re := regexp.MustCompile("(\\[|\\().+?(\\]|\\))") // (.*) [.*]
-	name = re.ReplaceAllString(name, "")
-	re = regexp.MustCompile("(?i)f[uc*]{2}k") // fuck
-	name = re.ReplaceAllString(name, "")
-
-	artist := track.Artist
-	artist = strings.Replace(artist, "!", "i", -1) // P!nk
-
-	return &Track{
-		Name:   name,
-		Artist: artist,
-	}
-}
-
-func (track Track) FileName() *Track {
-	converted := track.Convert()
-	return &Track{
-		Name:   lowerCaseNoSpace(converted.Name),
-		Artist: lowerCaseNoSpace(converted.Artist),
-	}
-}
-
-func (track Track) Query() string {
-	if &track == nil {
-		return ""
-	}
-	converted := track.Convert()
-	return (*converted).Name + " " + (*converted).Artist
-}
-
-func (trackA Track) Equal(trackB Track) bool {
-	return trackA.Name == trackB.Name && trackA.Artist == trackB.Artist
-}
-
-func (trackA Track) NotEqual(trackB Track) bool {
-	return !trackA.Equal(trackB)
-}
 
 func getCurrentTrack() bool {
 	var output bytes.Buffer
@@ -126,7 +73,7 @@ func getCurrentTrack() bool {
 
 	info := strings.Split(out, "\n")
 
-	newTrack := &Track{
+	newTrack := &structs.Track{
 		Name:   info[0],
 		Artist: info[1],
 	}
@@ -141,96 +88,8 @@ func getCurrentTrack() bool {
 	return false
 }
 
-func findOnAZLyrics(__query__ string) []Result {
-	results := []Result{}
-	if __query__ == "" {
-		return results
-	}
-	query := url.Values{}
-	query.Add("q", __query__)
-	URL := url.URL{
-		Scheme:   "http",
-		Host:     "search.azlyrics.com",
-		Path:     "search.php",
-		RawQuery: query.Encode(),
-	}
-	doc, err := goquery.NewDocument(URL.String())
-	if err != nil {
-		errorln("Failed to get lyrics.")
-		return results
-	}
-	doc.Find("a").Each(func(i int, anchor *goquery.Selection) {
-		href, _ := anchor.Attr("href")
-		if strings.HasPrefix(href, "http://www.azlyrics.com/lyrics/") {
-			results = append(results, Result{
-				URL:       href,
-				TrackName: anchor.Text(),
-			})
-		}
-	})
-	return results
-}
-
-func findOnAZLyricsByTrack(track *Track) []Result {
-	return findOnAZLyrics((*track).Query())
-}
-
-func getLyrics(lyricsURL string) []byte {
-	songPage, err := goquery.NewDocument(lyricsURL)
-	if err != nil {
-		errorln("Failed to get lyrics.")
-		return []byte{}
-	}
-	song := strings.TrimSpace(songPage.Find("#main > b").First().Text())
-	artist := songPage.Find("#main > h2").First().Text()
-	artist = strings.Replace(artist, "LYRICS", "", -1)
-	artist = strings.TrimSpace(artist)
-	lyrics := strings.TrimSpace(songPage.Find("#main > div[style]").Text())
-	return []byte(fmt.Sprintf("%s by %s\n\n%s", song, artist, lyrics))
-}
-
 func lowerCaseNoSpace(input string) string {
 	return strings.Replace(strings.ToLower(input), " ", "", -1)
-}
-
-func filterPossibleResultByTrack(results *[]Result, track *Track) string {
-	if len(*results) < 1 {
-		return ""
-	}
-	var index int = 0
-	for i, result := range *results {
-		if lowerCaseNoSpace(result.TrackName) == lowerCaseNoSpace((*track).Name) {
-			index = i
-			break
-		}
-	}
-	return (*results)[index].URL
-}
-
-func score(long, short string) float64 {
-	return 1 - float64(len(strings.Replace(long, short, "", -1)))/float64(len(long))
-}
-
-// you can test with `lyrics --no-pager Mean Pink`
-func filterPossibleResultByQuery(results *[]Result, query string) string {
-	if len(*results) < 1 {
-		return ""
-	}
-	var index int = 0
-	var max float64
-	parts := strings.Split(query, " ")
-
-	for i, result := range *results {
-		for _, part := range parts {
-			per := score(lowerCaseNoSpace(result.TrackName), lowerCaseNoSpace(part))
-			if per > max {
-				max = per
-				index = i
-			}
-		}
-	}
-
-	return (*results)[index].URL
 }
 
 func errorln(a ...interface{}) {
@@ -287,15 +146,15 @@ func trapCtrlC() {
 }
 
 func findLyrics() {
-	var results []Result
+	var results structs.Results
 	var link, dir, filename string
 	var lyrics []byte
 	var err error
 	var needToGetLyrics bool = true
 
 	if hasStartupQuery {
-		results = findOnAZLyrics(startupQuery)
-		link = filterPossibleResultByQuery(&results, startupQuery)
+		results = azlyrics.Search(startupQuery)
+		link = results.FilterByQuery(startupQuery)
 	} else {
 		f := (*currentTrack).FileName()
 		dir = path.Join(lyricsCacheDir, f.Artist)
@@ -304,13 +163,13 @@ func findLyrics() {
 		if err == nil {
 			needToGetLyrics = false
 		} else {
-			results = findOnAZLyricsByTrack(currentTrack)
-			link = filterPossibleResultByTrack(&results, currentTrack)
+			results = azlyrics.SearchByTrack(currentTrack)
+			link = results.FilterByTrack(currentTrack)
 		}
 	}
 
 	if needToGetLyrics && link != "" {
-		lyrics = getLyrics(link)
+		lyrics = azlyrics.GetLyrics(link)
 
 		if dir != "" && filename != "" {
 			err = os.MkdirAll(dir, 0755)
